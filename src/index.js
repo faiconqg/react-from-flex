95932e13b1c86c03e376853c87cdeb3c2b8da7b0
Thiago Lins <thiagolins@faicon.com>
2017-12-07 16:42:31 -0200

    all: first steps

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-export {Label} from './controls/Label';
-export {Spacer} from './controls/Spacer';
-export {Validator} from './engine/Validator';
-export {HGroup} from './layout/HGroup';
-export {VGroup} from './layout/VGroup';
-export {DateUtil} from './util/DateUtil';
-export {FormatUtil} from './util/FormatUtil';
+export {default as Label} from './controls/Label';
+export {default as Spacer} from './controls/Spacer';
+export {default as Validator} from './engine/Validator';
+export {default as HGroup} from './layout/HGroup';
+export {default as VGroup} from './layout/VGroup';
+export {default as DateUtil} from './util/DateUtil';
+export {default as FormatUtil} from './util/FormatUtil';
